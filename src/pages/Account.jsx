210303c54be46ec082148e4ff72b4233440d33ef
Rusul10 <rusul10amer@gmail.com>
2023-10-20 14:11:58 +0300

    auth

--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -2,10 +2,18 @@ import React from 'react'
 import {auth} from '../firebaseConfig'
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/Account.css'
 export default function Account() {
+  const nav= useNavigate();
+  const [name,setName]= useState('');
   const [email,setEmail]= useState('');
   const [password,setPassword]= useState('');
+
+  const handleSubmit = (e) => {e.preventDefault();
+  
+    nav("/")
+  }; 
 
   const signIn = async() => {
     await createUserWithEmailAndPassword(auth,email,password);
@@ -15,8 +23,14 @@ export default function Account() {
     <div className='account-comp'>
     <div className='account'>
       <div >
-          <form className='signUp-form'>
+          <form className='signUp-form' onSubmit={handleSubmit}>
             <h2>Sign up</h2>
+            <div className='Field'>
+                <label>
+                  Name:
+                </label>
+                <input type="text" onChange={(e) => {setName(e.target.value)}}/>
+            </div>
             <div className='field'>
               <label>
                 Email:
